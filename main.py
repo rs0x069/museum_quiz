447d492d12249d4e491735e92c071c2a047da85b
Ramil Safin <rs0x069@gmail.com>
2022-07-12 19:53:41 +0300

    Add handler ConversationHandler

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ import random
 import re
 import redis
 
+from enum import Enum, unique
+from functools import partial
+
 from dotenv import load_dotenv
-from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+
+reply_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счёт']]
+reply_markup = ReplyKeyboardMarkup(reply_keyboard)
+
+
+@unique
+class Quiz(Enum):
+    NEW_QUESTION = 1
+    ANSWER = 2
+    GIVING_UP = 3
+    SCORE = 4
 
 
 def load_questions():
@@ -43,11 +57,11 @@ def load_questions():
                     # Method 2 to find answer
                     if re.search(r'^Ответ:$', line):
                         answer = readable_file.readline().rstrip()
-                        answer = answer.translate({ord(c): None for c in '."[]'}).strip().lower()
+                        answer_lowercase = answer.translate({ord(c): None for c in '."[]'}).strip().lower()
 
                         quiz_questions.append({
                             'Вопрос': question,
-                            'Ответ': answer
+                            'Ответ': answer_lowercase
                         })
     except OSError as err:
         print(f'OSError: {err}')
@@ -55,39 +69,92 @@ def load_questions():
         return quiz_questions
 
 
+def get_random_quiz_question():
+    quiz_questions = load_questions()
+    random_number_questions = random.randint(0, len(quiz_questions) - 1)
+    return quiz_questions[random_number_questions]['Вопрос']
+
+
+def get_correct_answer(wished_question):
+    for quiz_question in load_questions():
+        if quiz_question['Вопрос'] == wished_question:
+            return quiz_question['Ответ']
+    return None
+
+
 def start_command(update, context):
-    custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счёт']]
-    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
-
     update.message.reply_text('Привет! Я бот для викторин!', reply_markup=reply_markup)
+    return Quiz.NEW_QUESTION
 
 
-def quiz(update, context):
-    custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счёт']]
-    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+def handle_new_question_request(update, context, db_redis):
+    user_id = update.message.from_user.id
 
-    redis_host = os.getenv("REDIS_HOST")
-    redis_port = os.getenv("REDIS_PORT")
-    redis_username = os.getenv("REDIS_USERNAME")
-    redis_password = os.getenv("REDIS_PASSWORD")
+    wished_question = db_redis.get(user_id)
+    if wished_question:
+        update.message.reply_markdown(f'Нужно ответить на загаданный вопрос. Сдаёшься?\n`{wished_question}`',
+                                      reply_markup=reply_markup)
+        return Quiz.ANSWER
 
-    db_redis = redis.Redis(
-        host=redis_host,
-        port=int(redis_port),
-        db=0,
-        username=redis_username,
-        password=redis_password,
-        decode_responses=True
-    )
+    random_quiz_question = get_random_quiz_question()
+    db_redis.set(user_id, random_quiz_question)
 
+    update.message.reply_text(random_quiz_question, reply_markup=reply_markup)
+
+    return Quiz.ANSWER
+
+
+def handle_solution_attempt(update, context, db_redis):
+    user_id = update.message.from_user.id
+
+    wished_question = db_redis.get(user_id)
+    if not wished_question:
+        update.message.reply_text('Для следующего вопроса нажми "Новый вопрос"', reply_markup=reply_markup)
+        return Quiz.NEW_QUESTION
+
+    user_answer = ''.join(update.message.text).lower()
+    correct_answer = get_correct_answer(wished_question)
+
+    if user_answer == correct_answer:
+        quiz_response = 'Правильно! Поздравляю! Для следующего вопроса нажми "Новый вопрос"'
+        db_redis.delete(user_id)
+        update.message.reply_text(quiz_response, reply_markup=reply_markup)
+        return Quiz.NEW_QUESTION
+    else:
+        quiz_response = 'Неправильно… Попробуешь ещё раз?'
+        update.message.reply_text(quiz_response, reply_markup=reply_markup)
+        return Quiz.ANSWER
+
+
+def handle_giving_up(update, context, db_redis):
+    user_id = update.message.from_user.id
+
+    wished_question = db_redis.get(user_id)
+    if not wished_question:
+        update.message.reply_text('Вопрос не задан. Для следующего вопроса нажми "Новый вопрос"',
+                                  reply_markup=reply_markup)
+        return Quiz.NEW_QUESTION
+
+    correct_answer = get_correct_answer(wished_question)
+    update.message.reply_text(f'Правильный ответ: {correct_answer}', reply_markup=reply_markup)
+
+    random_quiz_question = get_random_quiz_question()
+    db_redis.set(user_id, random_quiz_question)
+    update.message.reply_text(f'Новый вопрос: {random_quiz_question}', reply_markup=reply_markup)
+
+    return Quiz.ANSWER
+
+
+def quiz(update, context, db_redis):
     user_id = update.message.from_user.id
 
     if update.message.text == 'Новый вопрос':
         wished_question = db_redis.get(user_id)
 
         if wished_question:
-            return update.message.reply_text('Нужно ответить на загаданный вопрос. Сдаёшься?',
-                                             reply_markup=reply_markup)
+            return update.message.reply_markdown(
+                f'Нужно ответить на загаданный вопрос. Сдаёшься?\n`{wished_question}`',
+                reply_markup=reply_markup)
 
         quiz_questions = load_questions()
         random_number_questions = random.randint(0, len(quiz_questions) - 1)
@@ -109,12 +176,7 @@ def quiz(update, context):
             return update.message.reply_text('Для следующего вопроса нажми "Новый вопрос"', reply_markup=reply_markup)
 
         user_answer = ''.join(update.message.text).lower()
-
-        correct_answer = ''
-        for quiz_question in load_questions():
-            if quiz_question['Вопрос'] == wished_question:
-                correct_answer = quiz_question['Ответ']
-                break
+        correct_answer = get_correct_answer(wished_question)
 
         if user_answer == correct_answer:
             quiz_response = 'Правильно! Поздравляю! Для следующего вопроса нажми "Новый вопрос"'
@@ -123,6 +185,13 @@ def quiz(update, context):
             quiz_response = 'Неправильно… Попробуешь ещё раз?'
 
     update.message.reply_text(quiz_response, reply_markup=reply_markup)
+
+
+def cancel_command(update, context):
+    update.message.reply_text(
+        'До свидания! Попробуйте пройти викторину ещё раз.', reply_markup=ReplyKeyboardRemove()
+    )
+    return ConversationHandler.END
 
 
 def main():
@@ -143,13 +212,34 @@ def main():
         decode_responses=True
     )
 
-    for item in load_questions():
-        print(item['Ответ'])
-
     updater = Updater(telegram_token)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', start_command))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, quiz))
+
+    # quiz_redis = partial(quiz, db_redis=db_redis)
+    handle_new_question_request_redis = partial(handle_new_question_request, db_redis=db_redis)
+    handle_solution_attempt_redis = partial(handle_solution_attempt, db_redis=db_redis)
+    handle_giving_up_redis = partial(handle_giving_up, db_redis=db_redis)
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start_command)],
+        states={
+            Quiz.NEW_QUESTION: [MessageHandler(Filters.regex('^Новый вопрос$'), handle_new_question_request_redis)],
+            Quiz.ANSWER: [
+                MessageHandler(
+                    Filters.text &
+                    ~(Filters.command | Filters.regex('^Сдаться$') | Filters.regex('^Мой счёт$') | Filters.regex(
+                        '^Новый вопрос$')), handle_solution_attempt_redis),
+                MessageHandler(Filters.regex('^Новый вопрос$'), handle_new_question_request_redis),
+                MessageHandler(Filters.regex('^Сдаться$'), handle_giving_up_redis)
+            ],
+            # Quiz.GIVING_UP: [MessageHandler(Filters.regex('^Сдаться$'), handle_giving_up_redis)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel_command)]
+    )
+    dispatcher.add_handler(conv_handler)
+
+    # dispatcher.add_handler(CommandHandler('start', start_command))
+    # dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, quiz_redis))
 
     updater.start_polling()
     updater.idle()
