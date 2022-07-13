27b1f666f3d4e16bf5ca7c4001129a55d15f69fe
Ramil Safin <rs0x069@gmail.com>
2022-07-13 17:58:53 +0300

    Add keyboard

--- bot_vk.py
+++ bot_vk.py
@@ -4,14 +4,22 @@ import random
 import vk_api as vk
 
 from dotenv import load_dotenv
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
+
+keyboard = VkKeyboard(one_time=True)
+keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
+keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
+keyboard.add_line()
+keyboard.add_button('Мой счёт', color=VkKeyboardColor.POSITIVE)
 
 
 def echo_message(event, vk_api):
     vk_api.messages.send(
         user_id=event.user_id,
         message=event.text,
-        random_id=random.randint(1, 1000)
+        random_id=random.randint(1, 1000),
+        keyboard=keyboard.get_keyboard()
     )
 
 
@@ -26,7 +34,6 @@ def main():
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            print(f'{event.text=}')
             echo_message(event, vk_api)
 
 
